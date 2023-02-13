aecb783cec0e3375b018feee84f9d357d794c7e8
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-02-13 17:15:31 -0300

    feat: adiciona classe abstrata para base dos processos de ODM

---
+++ src/Models/AbstractODM.ts
@@ -0,0 +1,19 @@
+import { model, Model, models, Schema } from 'mongoose';
+
+abstract class AbstractODM<T> {
+  protected model: Model<T>;
+  protected schema: Schema;
+  protected modelName: string;
+
+  constructor(schema: Schema, modelName: string) {
+    this.schema = schema;
+    this.modelName = modelName;
+    this.model = models[this.modelName] || model(this.modelName, this.schema);
+  }
+
+  public async create(obj: T): Promise<T> {
+    return this.model.create({ ...obj });
+  }
+}
+
+export default AbstractODM;

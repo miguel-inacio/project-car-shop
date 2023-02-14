5dc79a01e5da14e344a1a5c3f939dc13c6e747bb
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-02-14 11:25:13 -0300

    feat: adiciona middleware de erro às rotas de carro

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import carRoutes from './Routes/CarRoutes';
+import CarValidation from './Middlewares/CarValidation';
 
 const app = express();
 app.use(express.json());
 
 app.use('/cars', carRoutes);
+
+app.use(CarValidation.notFound);
 
 export default app;

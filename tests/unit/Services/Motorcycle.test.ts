c759c8cbba5ea02cab57968a6753c828d634a36d
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-02-14 16:32:37 -0300

    test: adiciona teste para registro de moto no banco de dados

---
+++ tests/unit/Services/Motorcycle.test.ts
@@ -0,0 +1,40 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import MotorcycleService from '../../../src/Services/Motorcycle';
+
+describe('Ao tentar', function () {
+  describe('cadastrar uma moto', function () {
+    it('deve retornar informações da moto cadastrada', async function () {
+      const bikeResultMock = {
+        id: '6348513f34c397abcad040b2',
+        model: 'Honda Cb 600f Hornet',
+        year: 2005,
+        color: 'Yellow',
+        status: true,
+        buyValue: 30.000,
+        category: 'Street',
+        engineCapacity: 600,
+      };
+  
+      const bikeInputMock = {
+        model: 'Honda Cb 600f Hornet',
+        year: 2005,
+        color: 'Yellow',
+        status: true,
+        buyValue: 30.000,
+        category: 'Street',
+        engineCapacity: 600,
+      };
+  
+      sinon.stub(Model, 'create').resolves(bikeResultMock);
+  
+      const service = new MotorcycleService();
+      const result = await service.register(bikeInputMock);
+  
+      expect(result).to.deep.equal(bikeResultMock);
+
+      sinon.restore();
+    });
+  });
+});

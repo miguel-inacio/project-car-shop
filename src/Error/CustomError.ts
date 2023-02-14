20cb36b2a883477fd59d676b829462611d69078f
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-02-14 12:35:32 -0300

    refactor: adiciona propriedade type ao CustomError

--- src/Error/CustomError.ts
+++ src/Error/CustomError.ts
@@ -1,10 +1,17 @@
 export default class CustomError extends Error {
   private _status: number;
-  constructor(message: string, status: number) {
+  private _type: string;
+  
+  constructor(message: string, status: number, type: string) {
     super(message);
     this._status = status;
+    this._type = type;
   }
   public getStatus() : number {
     return this._status;
+  }
+
+  public getType() : string {
+    return this._type;
   }
 }

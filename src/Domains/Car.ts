37f78993bad75791a33c522dbdc4ff39d7d8a308
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-02-14 16:24:40 -0300

    refactor: reafatora classe filha Car

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,22 +1,12 @@
 import ICar from '../Interfaces/ICar';
+import Vehicle from './Vehicle';
 
-export default class Car {
-  protected id?: string | undefined;
-  protected model: string;
-  protected year: number;
-  protected color: string;
-  protected status?: boolean | undefined;
-  protected buyValue: number;
+export default class Car extends Vehicle {
   private doorsQty: number;
   private seatsQty: number;
 
   constructor(newCar: ICar) {
-    this.id = newCar.id;
-    this.model = newCar.model;
-    this.year = newCar.year;
-    this.color = newCar.color;
-    this.status = !!newCar.status;
-    this.buyValue = newCar.buyValue;
+    super(newCar);
     this.doorsQty = newCar.doorsQty;
     this.seatsQty = newCar.seatsQty;
   }

efa7f321b37dc0b4151b28a844815ea23aa3deff
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-02-14 19:24:01 -0300

    feat: adiciona rota DELETE/id para carros

--- src/Routes/CarRoutes.ts
+++ src/Routes/CarRoutes.ts
@@ -23,4 +23,9 @@ carRoutes.put(
   (req, res, next) => new CarController(req, res, next).update(),
 );
 
+carRoutes.delete(
+  '/:id',
+  (req, res, next) => new CarController(req, res, next).delete(),
+);
+
 export default carRoutes;

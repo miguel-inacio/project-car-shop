17bb45066f486fdc4918e8e932714fcec9ffa9e5
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-02-14 12:24:42 -0300

    refactor: remoção do arquivo das factories

--- src/Factories/CarFactory.ts
+++
@@ -1,7 +0,0 @@
-import CarController from '../Controllers/CarController';
-import CarService from '../Services/CarService';
-
-const carService: CarService = new CarService();
-const carController: CarController = new CarController(carService);
-
-export default { carController };

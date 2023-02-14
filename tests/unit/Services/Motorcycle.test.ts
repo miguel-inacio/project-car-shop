9b18733863b72b2a2ffa8f8cc41a3a73bc0da5fd
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-02-14 18:00:13 -0300

    test: adiciona teste para mongo id inválido

--- tests/unit/Services/Motorcycle.test.ts
+++ tests/unit/Services/Motorcycle.test.ts
@@ -96,5 +96,15 @@ describe('Ao tentar', function () {
 
       sinon.restore();
     });
+
+    it('deve retornar com INVALID MONGO ID quando passado id inválido', async function () {
+      try {
+        const service = new MotorcycleService();
+        await service.findOne('INVALID_MONGO_ID');
+      } catch (error) {
+        expect((error as CustomError).message).to.be.equal('Invalid mongo id');
+        expect((error as CustomError).getStatus()).to.be.equal(422);
+      }
+    });
   });
 });

cb9b0d2d16ed212e3404c226d8929b58a73b1df6
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-02-14 16:23:56 -0300

    refactor: reafatora classe filha Motorcycle

--- src/Domains/Motorcycle.ts
+++ src/Domains/Motorcycle.ts
@@ -1,22 +1,12 @@
 import IMotorcycle from '../Interfaces/IMotorcycle';
+import Vehicle from './Vehicle';
 
-export default class Motorcycle {
-  protected id?: string | undefined;
-  protected model: string;
-  protected year: number;
-  protected color: string;
-  protected status?: boolean;
-  protected buyValue: number;
+export default class Motorcycle extends Vehicle {
   private category: string;
   private engineCapacity: number;
 
   constructor(newBike: IMotorcycle) {
-    this.id = newBike.id;
-    this.model = newBike.model;
-    this.year = newBike.year;
-    this.color = newBike.color;
-    this.status = !!newBike.status;
-    this.buyValue = newBike.buyValue;
+    super(newBike);
     this.category = newBike.category;
     this.engineCapacity = newBike.engineCapacity;
   }

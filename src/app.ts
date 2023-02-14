a2aec2afe6f87a2c6fcf95cc1ff2b1dd3a2f92cd
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-02-14 16:50:14 -0300

    refactor: adiciona rotas de /motorcycles à aplicação

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import carRoutes from './Routes/CarRoutes';
 import CarValidation from './Middlewares/CarValidation';
+import motorcycleRoutes from './Routes/MotorcycleRoutes';
 
 const app = express();
 app.use(express.json());
 
 app.use('/cars', carRoutes);
+
+app.use('/motorcycles', motorcycleRoutes);
 
 app.use(CarValidation.notFound);
 
